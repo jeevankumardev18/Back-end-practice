aaa74c777d7bed7d5608dcff4399706f28fa572a
B Jeevan Kumar <jeevankumardev18@gmail.com>
2026-03-02 14:22:49 +0530

    Intial Commit

--- src/main/java/com/wrogn/task/controller/UserController.java
+++ src/main/java/com/wrogn/task/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.wrogn.task.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wrogn.task.dto.UserDto;
 import com.wrogn.task.dto.UserRequestDto;
 import com.wrogn.task.dto.UserResponseDto;
-import com.wrogn.task.exceptions.ResourceNotFoundException;
+import com.wrogn.task.service.UserService;
 
 import jakarta.validation.Valid;
 
@@ -25,49 +25,55 @@ import jakarta.validation.Valid;
 public class UserController 
 {
 	
+	private final UserService service;
+	
+	public UserController(UserService service)
+	{
+		this.service=service;
+	}
 	
 	@PostMapping
 	public ResponseEntity<String> createUser(@Valid @RequestBody UserRequestDto request)
 	{
-		//UserResponseDto response = new UserResponseDto();
-		//response.setId(1L);
-		//response.setEmail(request.getEmail());
-		//return response;
-		//return ResponseEntity.status(HttpStatus.CREATED).body(response);
-		
-		return ResponseEntity.ok("Login Sucessfully");
+				
+		 service.createUser(request);
+		return ResponseEntity.ok("User Created Sucessfully");
 	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id)
 	{
-		if(id != 1 && id!=2 && id!=3 )
-		{
-			throw new ResourceNotFoundException("User not found with id " + id);
-		}
-		 UserResponseDto dto = new UserResponseDto();
-		    dto.setId(1L);
-		    dto.setEmail("test@gmail.com");
 
-		    return ResponseEntity.ok(dto);
+		    return ResponseEntity.ok(service.getUserById(id));
 	}
 
 	@GetMapping
-	public ResponseEntity<List<UserDto>> getAll()
+	public ResponseEntity<List<UserResponseDto>> getAll()
 	{
-		return ResponseEntity.ok(new ArrayList<>());
+		return ResponseEntity.ok(service.getAllUsers());
 	}
 	
 	
 	@PutMapping("/{id}")
-	public ResponseEntity<String> updateUser(@PathVariable Long id,@RequestBody UserDto dto)
+	public ResponseEntity<String> updateUser(@PathVariable Long id,@RequestBody UserRequestDto dto)
 	{
+		 service.updateUser(id, dto);
 		return ResponseEntity.ok("User Updated Sucessfully");
 	}
 	
 	@DeleteMapping("/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable Long id) 
 	{
+		service.deleteUser(id);
         return ResponseEntity.ok("User deleted successfully");
     }
+	
+	@GetMapping("/pagination")
+	public ResponseEntity<Page<UserResponseDto>> getAllUsers(@RequestParam(defaultValue = "0")int page,
+			                 @RequestParam(defaultValue = "5") int size,
+			                 @RequestParam(defaultValue = "id") String sortBy
+			)
+	{
+		return ResponseEntity.ok(service.getAllUsersWithPagination(page, size, sortBy));
+	}
 }
